f1c94f8bb9c961d8a20d710e280396bd1e8501ad
Nikita-Zoteev <71443621+Nikita-Zoteev@users.noreply.github.com>
2020-09-17 21:24:50 +0300

    Add files via upload

---
+++ Лабораторная 4/Лабораторная 4-3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	float a; float b;
+	cout << "Введите первое число: ";
+	cin >> a;
+	cout << "Введите второе число: ";
+	cin >> b;
+	cout << "Среднее арифметическое = " << (a+b)/2 << endl;
+}
+
