1ec5890bb740824cf1014915c0744f214dca25e9
Nikita-Zoteev <71443621+Nikita-Zoteev@users.noreply.github.com>
2020-09-17 20:44:45 +0300

    Add files via upload

---
+++ Лабораторная 5-1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int x1, x2, y1, y2;
+	cout << "Введите первую координату: ";
+	cin >> x1 >> y1;
+	cout << "Введите вторую координату: ";
+	cin >> x2 >> y2;
+	cout << "Расстояние между точками = " << sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2))<< endl;
+}
