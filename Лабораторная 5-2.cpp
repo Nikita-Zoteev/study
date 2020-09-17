495871235fa4f6770ceb77028f4c6e4a7af3a568
Nikita-Zoteev <71443621+Nikita-Zoteev@users.noreply.github.com>
2020-09-17 21:12:20 +0300

    Add files via upload

---
+++ Лабораторная 5-2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int A, B, C;
+	cout << "Введите A, B и C: ";
+	cin >> A >> B >> C;
+	cout << "AC = " << C - A << endl;
+	cout << "BC = " << C - B << endl;
+	cout << "AC + BC = " << C - A + C - B << endl;
+}
