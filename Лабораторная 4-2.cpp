442b1085a685dab63ed93e0c5317da6e90ad8f07
Nikita-Zoteev <71443621+Nikita-Zoteev@users.noreply.github.com>
2020-09-17 19:46:15 +0300

    Add files via upload

---
+++ Лабораторная 4-2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int d;
+	const double PI = 3.14;
+	cout << "Введите диаметр: ";
+	cin >> d;
+	cout << "L = " << PI * d << endl;
+}
