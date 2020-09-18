7f2c500a293997d589c35be2aa8f9761e44b7de4
Nikita-Zoteev <71443621+Nikita-Zoteev@users.noreply.github.com>
2020-09-18 09:17:10 +0300

    Add files via upload

---
+++ Лаборатоная 5/Лабораторная 5-3.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+	int A, B, C;
+	cout << "Введите A, C и B: ";
+	cin >> A >> C >> B;
+	cout << "AC * BC = " << abs((C - A) * (B - C)) << endl;
+}
+
---
+++ Лаборатоная 5/Лабораторная 5-4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int x1, x2, y1, y2, a, b, P, S;
+	cout << "Введите координаты точки A: ";
+	cin >> x1 >> y1;
+	cout << "Введите координаты точки C: ";
+	cin >> x2 >> y2;
+	a = abs(x2 - x1);
+	b = abs(y2 - y1);
+	P = 2 * (a + b);
+	S = a * b;
+	cout << "P = " << P << endl;
+	cout << "S = " << S << endl;
+}
+
